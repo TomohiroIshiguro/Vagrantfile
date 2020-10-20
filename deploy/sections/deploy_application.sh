5dca5ff0e769c9e4ab34d2bab8858a064ff454aa
TomohiroIshiguro <se.ishiguro.tomohiro@gmail.com>
2020-10-21 00:15:34 +0900

    DELETE install sdk config.

--- deploy/sections/deploy_application.sh
+++ deploy/sections/deploy_application.sh
@@ -2,58 +2,34 @@
 
 cd ${work_directory}
 
-# In case to create a new project
-# ----------------------------------------
-/usr/local/bin/composer create-project --prefer-dist laravel/laravel=5.8 ${app_folder}
-cd ${app_folder}
 
-# In case to clone an existing project
-# ----------------------------------------
-# git clone ${target_branch} ${repository_url} ${app_folder} <<-AGREE
-# yes
-# AGREE
-# cd ${app_folder}
-# /usr/local/bin/composer install
-
-sudo chmod -R 777 ./bootstrap/cache
-sudo chmod -R 777 ./storage
-
-sudo cp /vagrant/deploy/config/.env .
-
-php artisan key:generate
-
-# ----------------------------------------
-# Create Database tables
-# ----------------------------------------
-
-php artisan migrate --seed
 
 # ----------------------------------------
 # Set web server config
 # ----------------------------------------
 
-sudo cat <<-VHOST >> /etc/httpd/conf.d/vhost.conf
-User vagrant
-Group vagrant
-
-<VirtualHost *:80>
-  ServerName ${ip_address}
-  DocumentRoot ${work_directory}/${app_folder}/public
-  <Directory ${work_directory}/${app_folder}/public>
-    Options FollowSymLinks
-    AllowOverride ALL
-    Require all granted
-  </Directory>
-</VirtualHost>
-
-<VirtualHost *:80>
-  ServerName ${domain_name}
-  DocumentRoot ${work_directory}/${app_folder}/public
-  <Directory ${work_directory}/${app_folder}/public>
-    Options FollowSymLinks
-    AllowOverride ALL
-    Require all granted
-  </Directory>
-</VirtualHost>
-VHOST
-sudo systemctl restart httpd.service
+#sudo cat <<-VHOST >> /etc/httpd/conf.d/vhost.conf
+#User vagrant
+#Group vagrant
+#
+#<VirtualHost *:80>
+#  ServerName ${ip_address}
+#  DocumentRoot ${work_directory}/${app_folder}/public
+#  <Directory ${work_directory}/${app_folder}/public>
+#    Options FollowSymLinks
+#    AllowOverride ALL
+#    Require all granted
+#  </Directory>
+#</VirtualHost>
+#
+#<VirtualHost *:80>
+#  ServerName ${domain_name}
+#  DocumentRoot ${work_directory}/${app_folder}/public
+#  <Directory ${work_directory}/${app_folder}/public>
+#    Options FollowSymLinks
+#    AllowOverride ALL
+#    Require all granted
+#  </Directory>
+#</VirtualHost>
+#VHOST
+#sudo systemctl restart httpd.service
--- provision/sections/install_sdk.sh
+++ provision/sections/install_sdk.sh
@@ -1,39 +1,3 @@
 #!/bin/bash
 
-# If you don't choose php, could you remove this area.
 
-sudo yum localinstall -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum localinstall -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-sudo yum install -y yum-utils
-sudo yum-config-manager --enable remi-php74
-
-sudo yum update -y
-sudo yum install -y php \
-                    php-bcmath \
-                    php-cli \
-                    php-ctype \
-                    php-devel \
-                    php-json \
-                    php-mbstring \
-                    php-mysql \
-                    php-tokenizer \
-                    php-xml \
-                    php-pdo
-
-sudo cat <<-PHP_INI >> /etc/php.ini
-error_logs = /var/log/php_errors.log
-
-date.timezone = "Asia/Tokyo"
-mbstring.language = Japanese
-mbstring.internal_encoding = UTF-8
-
-memory_limit=1024M
-upload_max_filesize=512M
-post_max_size=1024M
-PHP_INI
-
-# Install composer
-# ----------------------------------------
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-sudo php -r "unlink('composer-setup.php');"
